09b9d519d45c3d490a624e41e08f00f2e55eaf97
Kelly <kellysue3@yahoo.com>
2024-12-13 10:04:25 -0700

    Added Migrations

--- TodoApi/DataAccess/ApplicationDbContext.cs
+++ TodoApi/DataAccess/ApplicationDbContext.cs
@@ -1,6 +1,13 @@
-﻿namespace TodoApi.DataAccess
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi.DataAccess
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        //Defines a constructor that accepts an instance of DbContextOptions<ApplicationDbContext>
+            //Used to configure the DbContext with settings like database provider/connection string
+        //DbContext class uses the options to configure itself ie, how it connects to database
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Models.TodoItemModel> TodoItems { get; set; }
     }
 }
--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,6 +1,14 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using TodoApi.DataAccess;
+using TodoApi.Repositories;
 
+var builder = WebApplication.CreateBuilder(args);
+//"ApplicationDbContextConnection refers to connection in appsettings.json
+var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 // Add services to the container.
+builder.Services.AddScoped<TodoItemRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
